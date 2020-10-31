1b13a113606c860ed659c0ba9a0051b5f40a0011
kaancembertas <kaancembertas@gmail.com>
2020-11-01 00:36:48 +0300

    update

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, request
 from constant import PATH
 import numpy as np
 from helper import success, badRequest
-from recognition import getFaceLocations,drawRectangle
+from recognition import getFaceLocations,drawRectangle,getFaceEncoding
 from cv2 import cv2
+import dlib
 
 app = Flask(__name__)
-
+print("USE GPU: "+str(dlib.DLIB_USE_CUDA))
 
 @app.route('/')
 def index():
@@ -15,7 +16,6 @@ def index():
 
 @app.route(PATH + '/checkFace', methods=['POST'])
 def checkFace():
-    print("checkFace")
     imageFile = request.files["image"].read()
     img = np.frombuffer(imageFile, np.uint8)
     img = cv2.imdecode(img, cv2.IMREAD_COLOR)
@@ -29,8 +29,9 @@ def checkFace():
 
     drawRectangle(img,faceLocations[0])
     cv2.imwrite("images/newImage.png",img)
+    faceEncoding = getFaceEncoding(img,faceLocations)
 
-    return success()
+    return success("OLDUOLDU")
 
 
 app.run(host="localhost", port=5000, debug=True)
